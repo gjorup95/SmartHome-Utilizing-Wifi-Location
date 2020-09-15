446aa1b5e798828db609cf7247012bea78043776
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2020-09-15 23:36:15 +0200

    k

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
+const { time } = require('console');
 const express = require('express')
-const {spawn} = require('child_process');
 const app = express()
-const port = 10500
+const port = 10503
 app.get('/', (req, res) => {
- 
- var dataToSend;
- // spawn new child process to call the python script
- const python = spawn('python', ['ultrasonic.py']);
- // collect data from script
- python.stdout.on('data', function (data) {
-  console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
- });
- // in close event we are sure that stream from child process is closed
- python.on('close', (code) => {
- console.log(`child process close all stdio with code ${code}`);
- // send data to browser
- res.send(dataToSend)
- });
- 
+
+    const { spawn } = require('child_process');
+    const pyProg = spawn('python', ['ultrasonic.py']);
+
+    pyProg.stdout.on('data', function(data) {
+
+        console.log(data.toString());
+        res.write(data);
+        res.end('end');    
+    });
+    
 })
-app.listen(port, () => console.log(`Example app listening on port 
-${port}!`))
+
+app.listen(port, () => console.log('Application listening on port 10503!'))
