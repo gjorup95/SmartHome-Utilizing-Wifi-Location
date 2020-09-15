7300f68d5c495577407bec1091e20d487a2c8861
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2020-09-15 22:18:54 +0200

    ksldæ

--- ultrasonic.py
+++ ultrasonic.py
@@ -44,6 +44,7 @@ if __name__ == '__main__':
         while True:
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
+            sys.stdout.flush()
             time.sleep(1)
 
         # Reset by pressing CTRL + C
