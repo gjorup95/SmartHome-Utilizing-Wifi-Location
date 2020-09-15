f34e4f4fcbfdcb0318921372ba0118a5e2f2d0ce
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2020-09-15 21:40:30 +0200

    d

---
+++ indexreal.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 10500
+app.get('/', (req, res) => {
+
+    const { spawn } = require('child_process');
+    const pyProg = spawn('python', ['ultrasonic.py']);
+
+    pyProg.stdout.on('data', function(data) {
+
+        console.log(data.toString());
+        res.write(data);
+        res.end('end');
+    });
+})
+
+app.listen(port, () => console.log('Application listening on port 10500!'))
--- ultrasonic.py
+++ ultrasonic.py
@@ -1,8 +1,3 @@
-python.stdout.on('data', function (data) {
-  console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
- });
- python.stdout.on('data', function (data) {
-  console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
- });
+import sys
+print(HejDetHerVirker)
+sys.stdout.flush()
