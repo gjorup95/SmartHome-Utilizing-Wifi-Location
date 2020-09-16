8eb0c77ec90545554a3ee4324bd37f85cd8bce5e
GjorupPi <gjollerup@gmail.com>
2020-09-16 02:16:36 +0200

    dsaklæ

--- blink.py
+++ blink.py
@@ -4,15 +4,21 @@ from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
-LED_STATE = False
-def blink():
-    while True:
-        GPIO.output(7, GPIO.HIGH)
-        LED_STATE = True
-        sleep(1)
-        GPIO.output(7, GPIO.LOW)
-        LED_STATE = False
-        sleep(1)
-        return LED_STATE
-print(blink())
-sys.stdout.flush()
+def off():
+    GPIO.output(7, GPIO.LOW)
+    LED_STATE = False
+    print(LED_STATE)
+    sys.stdout.flush()
+    sleep(2)
+def on():
+    GPIO.output(7, GPIO.HIGH)
+    LED_STATE = True
+    print(LED_STATE)
+    sys.stdout.flush()
+    sleep(2)
+while True:
+    on()
+    off()
+
+    
+
