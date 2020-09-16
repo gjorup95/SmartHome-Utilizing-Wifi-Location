6fa33fc02dcdb2ea6df0f647c8fd7b28424a4edf
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2020-09-16 15:44:59 +0200

    dasd

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const express = require('express');
 const app = express()
 const port = 10503
 const { spawn } = require('child_process');
-const pyBlink = spawn('python', ['blink.py']);
 var sensorLib = require('node-dht-sensor');
+var Gpio = require('onoff').Gpio; 
+var LED = new Gpio(11, 'out'); 
+var blinkInterval = setInterval(blinkLED, 500); 
+function blinkLED() {
+  if (LED.readSync() === 0) {
+    LED.writeSync(1); //set output to 1 i.e turn led on
+  } else {
+    LED.writeSync(0); //set output to 0 i.e. turn led off 
+ }
+}
+function endBlink() { 
+  clearInterval(blinkInterval); 
+  LED.writeSync(0); 
+
+  LED.unexport(); // Unexport GPIO to free resources
+}
+setTimeout(endBlink, 2000);
 var output ="";
 var dht_sensor = {
     initialize: function () {
@@ -12,7 +28,7 @@ var dht_sensor = {
     },
     read: function () {
         var readout = sensorLib.read();
-        output = "Temperature: '" + readout.temperature.toFixed(2) + "C, " + "humidity: " + readout.humidity.toFixed(2) + "%";
+        output = "Temperature: " + readout.temperature.toFixed(2) + "C, " + "humidity: " + readout.humidity.toFixed(2) + "%   -  ";
         console.log(output);
         setTimeout(function () {
             dht_sensor.read();
