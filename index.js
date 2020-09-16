202dbf5f5d82297de6fb4d90b9c6f7e960467c2a
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2020-09-16 15:22:34 +0200

    dasd

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const app = express()
 const port = 10503
 const { spawn } = require('child_process');
 const pyBlink = spawn('python', ['blink.py']);
-const humSensor = spawn('node.js', ['humsensor.js']);
+var sensorLib = require('node-dht-sensor');
+var output = "";
+var dht_sensor = {
+    initialize: function () {
+        return sensorLib.initialize(11, 4);
+    },
+    read: function () {
+        var readout = sensorLib.read();
+        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+            'humidity: ' + readout.humidity.toFixed(2) + '%');
+        setTimeout(function () {
+            dht_sensor.read();
+        }, 2000);
+    }
+};
+
+if (dht_sensor.initialize()) {
+     dht_sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
+
+
 app.get('/', (req, res) => {
     const pyUltra = spawn('python', ['ultrasonic.py'])
     pyUltra.stdout.on('data', function(data) {
