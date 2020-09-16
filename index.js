96387773632409b02acfd02ea31e11877c770d1b
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2020-09-16 13:23:55 +0200

    das

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const { time } = require('console');
 const express = require('express')
 const app = express()
 const port = 10503
+const { spawn } = require('child_process');
+const pyBlink = spawn('python', ['blink.py']);
 app.get('/', (req, res) => {
 
-    const { spawn } = require('child_process');
     const pyUltra = spawn('python', ['ultrasonic.py'])
     pyUltra.stdout.on('data', function(data) {
         console.log(data.toString());
