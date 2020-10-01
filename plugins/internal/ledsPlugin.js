f6286d53a3963bdb43548dd2ee2fe65d0815c3a9
GjorupPi <gjollerup@gmail.com>
2020-10-01 17:19:48 +0200

    fix

--- plugins/internal/ledsPlugin.js
+++ plugins/internal/ledsPlugin.js
@@ -1,3 +1,13 @@
+var connectionString = 'HostName=GjorupPi.azure-devices.net;DeviceId=GjorupPi001;SharedAccessKey=MYrYqGrUPYFcEymr8os4vNpGTTzvGSRGNf24F9S1j4U=';
+ 
+// use factory function from AMQP-specific package
+var clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
+ 
+// AMQP-specific factory function returns Client object from core package
+var client = clientFromConnectionString(connectionString);
+ 
+// use Message object from core package
+var Message = require('azure-iot-device').Message;
 var resources = require('./../../resources/model');
 var actuator, interval;
 var model = resources.pi.actuators.leds['1'];
@@ -12,6 +22,24 @@ alreadyPlayed = false;
 exports.start = function (params) {
   localParams = params;
   
+  var connectCallback = function (err) {
+    if (err) {
+      console.error('Could not connect: ' + err);
+    } else {
+      console.log('Client connected');
+      var msg = new Message('some data from my device');
+      client.sendEvent(msg, function (err) {
+        if (err) {
+          console.log(err.toString());
+        } else {
+          console.log('Message sent');
+        };
+      });
+    };
+  };
+   
+   
+  client.open(connectCallback);
   if (localParams.simulate) {
     simulate();
   } else {
--- plugins/internal/ultrasonicPlugin.js
+++ plugins/internal/ultrasonicPlugin.js
@@ -3,16 +3,17 @@ var resources = require('../../resources/model'),
   var sensor;
   var model = resources.pi.sensors;
   var pluginName = 'Ultrasonic distance sensor';
-    const Gpio = require('pigpio').Gpio;
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-exports.start = function (params){
+  const Gpio = require('pigpio').Gpio;
   const MICROSECDONDS_PER_CM = 1e6/34321;
   const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
   const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
-  
-  
-  
   trigger.digitalWrite(0); // Make sure trigger is low
+exports.start = function (params){
+  
+  
+  
+  
+  
   
   const watchHCSR04 = () => {
     let startTick;
@@ -41,4 +42,6 @@ exports.start = function (params){
 
 exports.stop = function(){
  clearInterval();
+ Gpio.terminate();
+ 
 }
